486785f7546c4f1cabe26b25e1aae157112e8b24
Gaurav29-08 <gauravsirari424@gmail.com>
2026-04-07 11:17:52 +0530

    add middleware for auth

--- src/app.js
+++ src/app.js
@@ -1,32 +1,19 @@
 const express = require("express");
 
+
 const app = express();
 
+const {adminAuth} = require('./middlewares/auth');
 
+app.use('/admin',adminAuth);
 
-app.get('/user/:userID/:name/:password',(req,res)=>{
-    console.log(req.params);
-    
-    
-    res.send({firstName:"Gaurav",lastName:"sirari"})
-})
-
-app.post("/user",(req,res)=>{
-    res.send("Data successfully saved");
-})
-
-app.delete('/user',(req,res)=>{
-    res.send("deleted successfully")
-})
-
-
-app.use('/test',(req,res)=>{
-    res.send("Hello testing");
-    
+app.get('/admin/getAllData',(req,res)=>{
+    res.send("data sent")
 });
 
-
-
+app.get('/admin/deleteData',(req,res)=>{
+    res.send("delete data")
+});
 
 
 
