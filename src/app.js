20fb4499f44f8c2b85f276016f15e894290494ae
Gaurav29-08 <gauravsirari424@gmail.com>
2026-04-09 10:47:23 +0530

    create a model and create an API post signup

--- src/app.js
+++ src/app.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const {connectDB}  = require("./config/database");
-
-
-
 const app = express();
+const User =  require("./models/user");
+
+
+app.post("/signup", async (req,res)=>{
+  const user = new User({
+    firstName : "Gaurav",
+    lastName : "Sirari",
+    emailId : "gauravsirari424@gmail.com",
+    password : "Gaurav@123",
+
+
+  });
+  await user.save();
+  res.send("User added successfully")
+})
 
 connectDB().then(()=>{
   console.log("Database connection established");
