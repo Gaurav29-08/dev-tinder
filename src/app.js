cae3703bc53bd38da4c13aace49914ab87e53585
Gaurav29-08 <gauravsirari424@gmail.com>
2026-04-09 14:16:53 +0530

    learn about some method findOne finadAll

--- src/app.js
+++ src/app.js
@@ -3,18 +3,45 @@ const {connectDB}  = require("./config/database");
 const app = express();
 const User =  require("./models/user");
 
+app.use(express.json());
+
 
 app.post("/signup", async (req,res)=>{
-  const user = new User({
-    firstName : "Gaurav",
-    lastName : "Sirari",
-    emailId : "gauravsirari424@gmail.com",
-    password : "Gaurav@123",
+  const user = new User(req.body);
 
 
-  });
-  await user.save();
+  try {
+     await user.save();
   res.send("User added successfully")
+
+  } catch(err){
+    res.status(400).send("Error saving the user : " + err.message);
+  }
+ 
+});
+
+app.get('/user',async (req,res)=>{
+  const userEmail = req.body.emailId;
+  try{
+    const users = await User.find({emailId:userEmail});
+    if(users.length===0){
+      res.status(404).send("User not found")
+    } else {
+      res.send(users);
+    }
+    } catch(err){
+      res.status(400).send("something went wrong")
+    }
+  
+});
+
+app.get('/feed', async (req,res)=>{
+  try{
+    const users = await User.find({});
+    res.send(users);
+  } catch {
+    res.status(400).send("something went wrong");
+  }
 })
 
 connectDB().then(()=>{
